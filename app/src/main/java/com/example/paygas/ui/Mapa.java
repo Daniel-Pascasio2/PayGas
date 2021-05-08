4dee7cff9e8104622f099696cbde74391a21c1ea
edwinpaz1995 <laureanopaz@hotmail.com>
2021-05-08 15:27:35 -0600

    Pantalla Gasolineras y Mapa

--- app/src/main/java/com/example/paygas/ui/Mapa.java
+++ app/src/main/java/com/example/paygas/ui/Mapa.java
@@ -1,9 +1,12 @@
 package com.example.paygas.ui;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +15,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.paygas.MapaActivity;
+import com.example.paygas.MenuUser;
 import com.example.paygas.R;
 
 public class Mapa extends Fragment {
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +27,27 @@ public class Mapa extends Fragment {
         View root = inflater.inflate(R.layout.fragment_mapa, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
 
+        Button btnMapa = (Button) root.findViewById(R.id.btnMapa);
+        btnMapa.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent h = new Intent(getActivity(), MapaActivity.class);
+                startActivity(h);
+            }
+        });
+
         return root;
+
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent h = new Intent(getActivity(), MapaActivity.class);
+        startActivity(h);
     }
 }
